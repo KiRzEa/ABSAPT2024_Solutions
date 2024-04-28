42b074510cb95541ab6a4797588f7cbc710c9c50
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-04-28 14:24:54 +0000

    debug

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -76,21 +76,18 @@ class ATEProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir, domain_type):
         """See base class."""
-        string = domain_type
         logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train2024.csv")))
         return self._create_examples(
             self._read_csv(os.path.join(data_dir, "train2024.csv")), "train")
 
     def get_valid_examples(self, data_dir, domain_type):
         """See base class."""
-        string = domain_type
         logger.info("LOOKING AT {}".format(os.path.join(data_dir, "task2_test.csv")))
         return self._create_examples(
             self._read_csv(os.path.join(data_dir, "task2_test.csv"), "valid"))
 
     def get_test_examples(self, data_dir):
         """See base class."""
-        string = domain_type
         logger.info("LOOKING AT {}".format(os.path.join(data_dir, "task1_test.csv")))
         return self._create_examples(
             self._read_csv(os.path.join(data_dir, "task1_test.csv")), "test")
@@ -101,7 +98,6 @@ class ATEProcessor(DataProcessor):
         seqlabs = [cls_token, 'B', 'I', 'O']
         label_list = []
         
-        label_list.append(sentiment)
         label_list.append(seqlabs)
         return label_list
 
@@ -121,7 +117,7 @@ class ATEProcessor(DataProcessor):
 
 
 def convert_examples_to_features(examples, label_list, max_seq_length,
-                                 tokenizer, output_mode, cls_token='[CLS]'):
+                                 tokenizer, cls_token='[CLS]'):
     """Loads a data file into a list of `InputBatch`s."""
 
     label_map_senti = {label : i for i, label in enumerate(label_list[0])}
@@ -197,8 +193,6 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
             logger.info("*** Example ***")
             logger.info("guid: %s" % (example.guid))
             logger.info("tokens_len: %s" % (tokens_len))
-            logger.info("guid: %s" % (exist_imp_aspect))
-            logger.info("guid: %s" % (exist_imp_opinion))
 
             logger.info("aspect tokens: %s" % " ".join(
                     [str(x) for x in aspect_tokens]))
@@ -214,9 +208,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
                     aspect_input_mask=aspect_input_mask,
                     aspect_ids=aspect_ids,
                     aspect_segment_ids=aspect_segment_ids,
-                    aspect_labels=aspect_labels,
-                    exist_imp_aspect=exist_imp_aspect,
-                    exist_imp_opinion=exist_imp_opinion))
+                    aspect_labels=aspect_labels))
     return features
 
 
@@ -234,117 +226,13 @@ def _truncate_seq_pair(bert_tokens_a, aspect_labels, max_length):
     bert_tokens_a.pop()
     aspect_labels.pop()
 
-
-def convert_examples_to_features2nd(examples, label_list, max_seq_length,
-                                 tokenizer, output_mode, cls_token='[CLS]'):
-    """Loads a data file into a list of `InputBatch`s."""
-
-    category_senti_map = {label : i for i, label in enumerate(label_list[0])}
-
-    features = []
-
-    for (ex_index, example) in enumerate(examples):
-        # pdb.set_trace()
-        if ex_index % 10000 == 0:
-            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
-
-        orig_tokens, ao_tags = example.text_a.strip().split('####')
-        # label for examples with negative samples
-        # labels = example.label[:-1]
-        orig_tokens = orig_tokens.split()
-        labels = example.label
-
-        bert_tokens_a = orig_tokens
-        bert_tokens_b = None
-
-        _truncate_seq_pair2nd(bert_tokens_a, max_seq_length - 2)
-
-        aspect_tokens = []
-        aspect_segment_ids = []
-
-        aspect_tokens.append(cls_token)
-        aspect_segment_ids.append(0)
-
-        for i, token in enumerate(bert_tokens_a):
-            aspect_tokens.append(token)
-            aspect_segment_ids.append(0)
-        aspect_tokens.append(cls_token)
-        tokens_len = len(aspect_tokens)
-        aspect_segment_ids.append(0)
-
-        aspect_input_ids = tokenizer.convert_tokens_to_ids(aspect_tokens)
-        # The mask has 1 for real tokens and 0 for padding tokens. Only real
-        # tokens are attended to.
-        aspect_input_mask = [1] * len(aspect_input_ids)
-        imp_opinion_pos = len(aspect_input_ids)
-        # if example.text_a.startswith('it has all the features that we'):
-        #   pdb.set_trace()
-
-        # Zero-pad up to the sequence length.
-        while len(aspect_input_ids) < max_seq_length:
-            aspect_input_ids.append(0)
-            aspect_input_mask.append(0)
-            aspect_segment_ids.append(0)
-
-        assert len(aspect_input_ids) == max_seq_length
-        assert len(aspect_input_mask) == max_seq_length
-        assert len(aspect_segment_ids) == max_seq_length
-
-        # get candidate aspect and opinion
-        label_id = [0] * len(label_list[0])
-        candidate_aspect = [0 for i in range(max_seq_length)]
-        candidate_opinion = [0 for i in range(max_seq_length)]
-        cur_aspect = ao_tags.split()[0]; cur_opinion = ao_tags.split()[1]
-        a_st = int(cur_aspect.split(',')[0]); a_ed = int(cur_aspect.split(',')[1])
-        o_st = int(cur_opinion.split(',')[0]); o_ed = int(cur_opinion.split(',')[1])
-        if a_st == -1:
-            a_ed = 0
-        if o_st == -1:
-            o_st = imp_opinion_pos - 2; o_ed = imp_opinion_pos - 1
-        for i in range(a_st+1, a_ed+1):
-            candidate_aspect[i] = 1
-        for i in range(o_st+1, o_ed+1):
-            candidate_opinion[i] = 1
-        if len(labels) > 0:
-            for ele in labels[0].split():
-                label_id[category_senti_map[ele]] = 1
-
-        if ex_index < 5:
-            logger.info("*** Example ***")
-            logger.info("guid: %s" % (example.guid))
-            logger.info("tokens_len: %s" % (tokens_len))
-
-            logger.info("aspect tokens: %s" % " ".join(
-                    [str(x) for x in aspect_tokens]))
-            logger.info("aspect_input_ids: %s" % " ".join([str(x) for x in aspect_input_ids]))
-            logger.info("aspect_input_mask: %s" % " ".join([str(x) for x in aspect_input_mask]))
-            logger.info(
-                    "aspect_segment_ids: %s" % " ".join([str(x) for x in aspect_segment_ids]))
-            logger.info(
-                    "candidate_aspect: %s" % " ".join([str(x) for x in candidate_aspect]))
-            logger.info(
-                    "candidate_opinion: %s" % " ".join([str(x) for x in candidate_opinion]))
-            logger.info(
-                    "label_id: %s" % " ".join([str(x) for x in label_id]))
-
-        features.append(
-                InputFeatures2nd(
-                    tokens_len=tokens_len,
-                    aspect_tokens=aspect_tokens,
-                    aspect_input_ids=aspect_input_ids,
-                    aspect_input_mask=aspect_input_mask,
-                    aspect_segment_ids=aspect_segment_ids,
-                    
-                    candidate_aspect=candidate_aspect,
-                    candidate_opinion=candidate_opinion,
-                    label_id=label_id,
-                    ))
-    return features
-
+def SOEProcessor(DataProcessor):
+    def __init__(self):
+        raise NotImplementedError()
 
 processors = {
-    "quad": QuadProcessor,
-    "categorysenti": CategorySentiProcessor,
+    "ate": ATEProcessor,
+    "soe": SOEProcessor,
 }
 
 output_modes = {
