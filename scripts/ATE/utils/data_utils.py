c0e999d2561188a174dea64dfb16f4bee3e02066
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-05-08 10:01:48 +0000

    debug

--- scripts/ATE/utils/data_utils.py
+++ scripts/ATE/utils/data_utils.py
@@ -108,7 +108,6 @@ def process(data_dir, tokenizer, level):
     ate_test_data['tokens'] = ate_test_data.apply(tokenize, axis=1)
 
     ate_train_data = ate_train_df.groupby('inputs').agg(list).reset_index()
-    print(ate_dev_df)
     ate_dev_data = ate_dev_df.groupby('inputs').agg(list).reset_index()
     
     train_ds = Dataset.from_pandas(pd.DataFrame(convert_to_bio(ate_train_data)))
