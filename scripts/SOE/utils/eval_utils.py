91be22a3211506dc836dfe5e10a8aa5f42aeab3b
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-05-21 17:00:34 +0700

    modify task 2's metrics

--- scripts/SOE/utils/eval_utils.py
+++ scripts/SOE/utils/eval_utils.py
@@ -16,11 +16,23 @@ def compute_metrics(p):
     mac_f1 = f1_score.compute(predictions=predictions, references=labels, average='macro')["f1"]
     mic_f1 = f1_score.compute(predictions=predictions, references=labels, average='micro')["f1"]
     weighted_f1 = f1_score.compute(predictions=predictions, references=labels, average='weighted')["f1"]
+    mac_p = precision.compute(predictions=predictions, references=labels, average='macro')["precision"]
+    mic_p = precision.compute(predictions=predictions, references=labels, average='micro')["precision"]
+    weighted_p = precision.compute(predictions=predictions, references=labels, average='weighted')["precision"]
+    mac_r = recall.compute(predictions=predictions, references=labels, average='macro')["recall"]
+    mic_r = recall.compute(predictions=predictions, references=labels, average='micro')["recall"]
+    weighted_r = recall.compute(predictions=predictions, references=labels, average='weighted')["recall"]
 
     return {
         "MacroF1": mac_f1,
+        "MacroPrecision": mac_p,
+        "MacroRecall": mac_r,
         "MicroF1": mic_f1,
+        "MicroPrecision": mic_p,
+        "MicroRecall": mic_r,
         "WeightedF1": weighted_f1,
+        "WeightedPrecision": weighted_p,
+        "WeightedRecall": weighted_r,
         "Accuracy": acc,
         "BalancedAccuracy": bal_acc
     }
